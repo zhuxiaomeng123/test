a355f7f72f6999cda15d612ad6cdd5438f37d0cb
mac zhuxiaomeng <1627135595@qq.com>
2018-07-31 17:49:54 +0800

    new file

---
+++ test/22-nodejs/mongo/003-mvc/Controller/Index.js
@@ -0,0 +1,14 @@
+/*
+* @Author: TomChen
+* @Date:   2018-07-28 10:38:06
+* @Last Modified by:   TomChen
+* @Last Modified time: 2018-07-28 11:22:32
+*/
+
+class Index{
+	index(){
+		console.log('Index index')
+	}
+}
+
+module.exports = new Index();
